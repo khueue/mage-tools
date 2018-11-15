fbc96d84b31518d64957971f87e0ed7b7859d41f
Sebastian Lundström <khueue@gmail.com>
2018-11-15 22:42:58 +0100

    More deploy stuff

--- scripts/app-deploy.sh
+++ scripts/app-deploy.sh
@@ -3,12 +3,13 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-set -o xtrace
+# set -o xtrace
 
 DIST_FOLDER=./app/_build/dist
 BUCKET=khueue-mage-tools-app
 
-# Upload everything (except index.html) with long TTL.
+echo
+echo "--- Upload all files except index.html ..."
 aws s3 cp \
 	--recursive \
 	--exclude index.html \
@@ -16,13 +17,29 @@ aws s3 cp \
 	${DIST_FOLDER} \
 	s3://${BUCKET}/
 
-# Upload index.html last, to make a smooth transition to the new version.
+echo
+echo "--- Request every file once, to fill the cache ..."
+for file in ${DIST_FOLDER}/*; do
+	basename=${file##*/}
+	echo "/${basename}"
+	curl \
+		--verbose \
+		--silent \
+		--header "Accept-Encoding: gzip, deflate, br" \
+		https://mage.khueue.com/${basename} \
+		2>&1 \
+	| grep x-cache
+done
+
+echo
+echo "--- Upload index.html last, to switch smoothly to new version ..."
 aws s3 cp \
 	--cache-control max-age=10 \
 	${DIST_FOLDER}/index.html \
 	s3://${BUCKET}/
 
-# Remove files from previous deploys.
+echo
+echo "--- Remove files from previous deploys ..."
 aws s3 sync \
 	--delete \
 	${DIST_FOLDER} \
